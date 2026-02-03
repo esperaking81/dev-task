0259cba14eaa28629ca523fbf9bc46e1ec1aa091
espera awo <esperaking81@gmail.com>
2026-02-03 03:39:44 +0000

    Fix postgres healthcheck and dev.fish shebang
    - Fix postgres healthcheck to use correct user (devtask_user instead of devtask_db)
    - Fix dev.fish shebang from /bin/bin/fish to /usr/bin/env fish

---
+++ dev.fish
@@ -0,0 +1,19 @@
+#!/usr/bin/env fish
+echo "Starting dev servers..."
+docker-compose -f compose.dev.yml --profile dev up -d
+
+if test $status -ne 0
+    echo "Starting dev servers... Failed"
+end
+
+echo "Starting dev servers... Done"
+
+echo "Launching frontend..."
+open http://localhost:3000
+echo "Launching frontend...Done"
+
+echo "Launching Swagger UI..."
+open http://localhost:4000/api
+echo "Launching Swagger UI...Done"
+
+echo "Dev environment is ready!"
